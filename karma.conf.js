0930276cb16c96bd0b41eab726e2c70c7b61682d
WandersonAlves <wanderson.alves@accenture.com>
2017-06-19 11:25:55 -0300

    First tests working

---
+++ karma.conf.js
@@ -0,0 +1,57 @@
+var webpackConfig = require('./webpack.config.js');
+delete webpackConfig.entry;
+
+// karma.conf.js
+module.exports = function (config) {
+  config.set({
+    browsers: ['PhantomJS'],
+
+    frameworks: ['jasmine'],
+
+    files: [
+      'tests/**/*.spec.js'
+    ],
+
+    // Vamos enviar os testes para o bundle
+    preprocessors: {
+      'src/**/*.(js|vue)': ['coverage'],
+      'tests/**/*.spec.js': ['webpack']
+    },
+
+    // Usando a configuração do Webpack
+    webpack: webpackConfig,
+
+    // Evitar paredes de informações
+    webpackMiddleware: {
+      noInfo: true
+    },
+
+    autoWatch: true,
+
+    singleRun: true,
+    reporters: ['coverage', 'spec'],
+		coverageReporter: {
+			type: "lcov",
+			dir: "reports",
+			subdir: "coverage",
+			check: {
+				global: {
+					statements: 75,
+					branches: 75,
+					functions: 75,
+					lines: 75
+				}
+			}
+		},
+		specReporter: {
+			suppressErrorSummary: true,
+			suppressFailed: false,
+			suppressPassed: false,
+			suppressSkipped: false
+		},
+		// plugins: [
+		// 	"karma-coverage",
+		// 	"karma-spec-reporter",
+		// ]
+  });
+};
---
+++ tests/App.vue.spec.js
@@ -0,0 +1,25 @@
+var App = require('../src/App.vue');
+
+describe('Main App', function () {
+
+  beforeAll(function() {
+
+  });
+
+  afterAll(function() {
+
+  });
+
+  it('Should have two components in App.vue', function() {
+    expect(App.components.NavigationBar).toBeDefined();
+    expect(App.components.GreyFooter).toBeDefined();
+  });
+
+  it('Should initialize "user" data with "Wanderson Alves"', function() {
+    expect(App.data().user).toBe('Wanderson Alves');
+  });
+
+  it('Should initialize "notifications" data with 4', function() {
+    expect(App.data().notifications).toBe(4);
+  });
+});
