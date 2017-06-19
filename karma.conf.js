3d4e2242b5ff00e3ce90a9df6d80fe5f537f9c64
WandersonAlves <wanderson.alves@accenture.com>
2017-06-19 11:39:31 -0300

    [WIP] Fix indent

--- karma.conf.js
+++ karma.conf.js
@@ -1,8 +1,7 @@
 var webpackConfig = require('./webpack.config.js');
 delete webpackConfig.entry;
 
-// karma.conf.js
-module.exports = function (config) {
+module.exports = function(config) {
   config.set({
     browsers: ['PhantomJS'],
 
@@ -11,47 +10,37 @@ module.exports = function (config) {
     files: [
       'tests/**/*.spec.js'
     ],
-
-    // Vamos enviar os testes para o bundle
     preprocessors: {
       'src/**/*.(js|vue)': ['coverage'],
       'tests/**/*.spec.js': ['webpack']
     },
-
-    // Usando a configuração do Webpack
     webpack: webpackConfig,
-
-    // Evitar paredes de informações
     webpackMiddleware: {
       noInfo: true
     },
-
     autoWatch: true,
-
     singleRun: true,
     reporters: ['coverage', 'spec'],
-		coverageReporter: {
-      reporters: [
-        { type: 'lcov', dir: 'reports', subdir: 'covarage' }
-      ],
-			check: {
-				global: {
-					statements: 75,
-					branches: 75,
-					functions: 75,
-					lines: 75
-				}
-			}
-		},
-		specReporter: {
-			suppressErrorSummary: true,
-			suppressFailed: false,
-			suppressPassed: false,
-			suppressSkipped: false
-		}
-		// plugins: [
-		// 	"karma-coverage",
-		// 	"karma-spec-reporter",
-		// ]
+    coverageReporter: {
+      reporters: [{
+        type: 'lcov',
+        dir: 'reports',
+        subdir: 'covarage'
+      }],
+      check: {
+        global: {
+          statements: 75,
+          branches: 75,
+          functions: 75,
+          lines: 75
+        }
+      }
+    },
+    specReporter: {
+      suppressErrorSummary: true,
+      suppressFailed: false,
+      suppressPassed: false,
+      suppressSkipped: false
+    }
   });
 };
